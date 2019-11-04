2dd42b0a20df20282d47996bd0e3ed825bdbab20
Chrismis79 <chrismis79@gmail.com>
2019-11-04 15:39:19 -0600

    Products refactored using Context API

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//Contexts
+import {ProductContext} from '../src/contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		return setCart([...cart, item]);
 	};
 
 	return (
+		<ProductContext.Provider value={[products, addItem]}>
 		<div className="App">
 			<Navigation cart={cart} />
 
@@ -23,10 +28,7 @@ function App() {
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+				component={Products}					
 					/>
 				)}
 			/>
@@ -36,6 +38,7 @@ function App() {
 				render={() => <ShoppingCart cart={cart} />}
 			/>
 		</div>
+		</ProductContext.Provider>
 	);
 }
 
